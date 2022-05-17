b846e09d95a72a605f8eb680a2ab5ced323249f5
meghthedev@gmail.com <megh@debian-BULLSEYE-live-builder-AMD64>
2022-05-17 09:47:38 +0530

    CPH1859: Initial Corvus Bringup Initial corvus device tree bringup for Realme 1 CPH1859

--- corvus_CPH1859.mk
+++ corvus_CPH1859.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2022 The LineageOS Project
+# Copyright (C) 2021-2022 The corvusOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,14 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from CPH1859 device
 $(call inherit-product, device/oppo/CPH1859/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common corvusOS stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
+CORVUS_MAINTAINER := Meghthedev
+RAVEN_LAIR := unofficial
+USE_GAPPS=true
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_CPH1859
+PRODUCT_NAME := corvus_CPH1859
 PRODUCT_DEVICE := CPH1859
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := realme 1
@@ -31,3 +35,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
 
 BUILD_FINGERPRINT := OPPO/CPH1859/CPH1859:9/PPR1.180610.011/1587903909:user/release-keys
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+# Pixel charging animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+# PixelExperience Stuffs
+TARGET_GAPPS_ARCH := arm64
