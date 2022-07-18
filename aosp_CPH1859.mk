d1ec3764d5c964632775c941e8fafd0faaadad77
arkmark99 <109340333+arkmark99@users.noreply.github.com>
2022-07-18 20:30:51 +0530

    Update and rename corvus_CPH1859.mk to aosp_CPH1859.mk

--- aosp_CPH1859.mk
+++ aosp_CPH1859.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from CPH1859 device
 $(call inherit-product, device/oppo/CPH1859/device.mk)
 
-# Inherit some common corvusOS stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -23,7 +23,7 @@ RAVEN_LAIR := Official
 USE_GAPPS=true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := corvus_CPH1859
+PRODUCT_NAME := aosp_CPH1859
 PRODUCT_DEVICE := CPH1859
 PRODUCT_BRAND := oppo
 PRODUCT_MODEL := realme 1
@@ -35,10 +35,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_oppo6771_17065-user 9 PPR1.180610.011 eng.root.20200509.060522 release-keys"
 
 BUILD_FINGERPRINT := OPPO/CPH1859/CPH1859:9/PPR1.180610.011/1587903909:user/release-keys
-
-# FaceUnlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-# Pixel charging animation
-TARGET_INCLUDE_PIXEL_CHARGER := true
-# PixelExperience Stuffs
-TARGET_GAPPS_ARCH := arm64
